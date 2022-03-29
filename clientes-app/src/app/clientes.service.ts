bc65d6b254fd69770c5f50f35553b56e6c82f0d0
Lucas Souza <lhenriquesouza00@gmail.com>
2022-03-29 08:17:47 -0300

    POST /api/clientes + CORS

--- clientes-app/src/app/clientes.service.ts
+++ clientes-app/src/app/clientes.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http'
+
 import { Cliente } from './clientes/cliente';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientesService {
 
-  constructor() { }
+  constructor( private http : HttpClient ) { }
+
+  salvar( cliente : Cliente ) : Observable<Cliente> {
+    return this.http.post<Cliente>('http://localhost:8080/api/clientes', cliente);
+  }
 
   getCliente() : Cliente {
     let cliente : Cliente = new Cliente();
