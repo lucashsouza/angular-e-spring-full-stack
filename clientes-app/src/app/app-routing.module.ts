15b287ed5fceeed6a1452583a4e7a9df43b60c39
Lucas Souza <lhenriquesouza00@gmail.com>
2022-05-08 15:53:05 -0300

    Create "/login" + URL fixing;

--- clientes-app/src/app/app-routing.module.ts
+++ clientes-app/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
+import { Template } from '@angular/compiler/src/render3/r3_ast';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
-  { path : 'home', component: HomeComponent }
+  { path : 'login', component: LoginComponent },
+  { path : '', component: LayoutComponent, children: [
+    { path : 'home', component: HomeComponent }
+  ]},
 ];
 
 @NgModule({
