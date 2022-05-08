9cab47b15c091de5cf7afbe8eb8d45241cf3ec72
Lucas Souza <lhenriquesouza00@gmail.com>
2022-05-08 15:53:05 -0300

    Create "/login" + URL fixing;

--- clientes-app/src/app/app.module.ts
+++ clientes-app/src/app/app.module.ts
@@ -13,15 +13,21 @@ import { ClientesService } from './clientes.service';
 import { ServicoPrestadoModule } from './servico-prestado/servico-prestado.module';
 
 import { ServicoPrestadoService } from './servico-prestado.service';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     HttpClientModule,
     AppRoutingModule,
     TemplateModule,
