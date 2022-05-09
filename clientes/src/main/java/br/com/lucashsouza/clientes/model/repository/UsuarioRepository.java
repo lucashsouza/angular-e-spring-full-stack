5c13a08ac4595aefebe25ceabc6b491c4a247363
Lucas Souza <lhenriquesouza00@gmail.com>
2022-05-09 08:53:47 -0300

    implementation OAuth + JWT

--- clientes/src/main/java/br/com/lucashsouza/clientes/model/repository/UsuarioRepository.java
+++ clientes/src/main/java/br/com/lucashsouza/clientes/model/repository/UsuarioRepository.java
@@ -3,6 +3,10 @@ package br.com.lucashsouza.clientes.model.repository;
 import br.com.lucashsouza.clientes.model.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    Optional<Usuario> findByUsername(String username);
 
 }
---
+++ clientes/src/main/java/br/com/lucashsouza/clientes/rest/UsuarioController.java
@@ -0,0 +1,23 @@
+package br.com.lucashsouza.clientes.rest;
+
+import br.com.lucashsouza.clientes.model.entity.Usuario;
+import br.com.lucashsouza.clientes.model.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/usuarios")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioRepository repository;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void salvar(@RequestBody @Valid Usuario usuario) {
+        repository.save(usuario);
+    }
+}
